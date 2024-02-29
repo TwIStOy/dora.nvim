b78c74d454c8849433a7a8c78e3f6ce939c57257
Hawtian Wang <twistoy.wang@gmail.com>
2024-02-29 15:19:48 +0800

    fix

--- lua/dora/packages/editor/plugins/_others.lua
+++ lua/dora/packages/editor/plugins/_others.lua
@@ -322,4 +322,18 @@ return {
       }
     end,
   },
+  {
+    "TwIStOy/nvim-lastplace",
+    event = "BufReadPre",
+    opts = {
+      lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+      lastplace_ignore_filetype = {
+        "gitcommit",
+        "gitrebase",
+        "svn",
+        "hgcommit",
+      },
+      lastplace_open_folds = false,
+    },
+  },
 }
