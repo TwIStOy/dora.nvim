90d651c19c5640fcc875a686cf8e7b8eee8f0526
Hawtian Wang <twistoy.wang@gmail.com>
2024-03-20 19:37:44 +0800

    feat: update rustaceanvim

--- lua/dora/packages/extra/lang/rust.lua
+++ lua/dora/packages/extra/lang/rust.lua
@@ -140,7 +140,7 @@ return {
     },
     {
       "mrcjkb/rustaceanvim",
-      version = "^3",
+      version = "^4",
       dependencies = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
       ft = { "rust" },
       actions = function()
